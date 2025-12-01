6dafb4ae355bc6508b06a624d487278284a66bc3
nabil4416 <nabil4416@hotmail.com>
2025-12-01 19:41:17 +0100

    update main 2

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include "get_next_line.h"
 
 //premier test: Pass an arbitrary file descriptor … on which it is not possible to read, for example 42. The function must return NULL.
-
+printf("=====First Test====");
 int main(void)
 {
     char *line;
@@ -18,6 +19,36 @@ int main(void)
     }
     return (0);
 }
+
+//deuxieme test:
+printf("=====Second Test====");
+int main(int argc, char **argv)
+{
+    int   fd;
+    char *line;
+    int   i = 0;
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <file>\n", argv[0]);
+        return (1);
+    }
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0)
+    {
+        perror("open");
+        return (1);
+    }
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("LINE %d: [%s]\n", i++, line);
+        free(line);
+    }
+    close(fd);
+    return (0);
+}
+
+
 
 /*int main(void)
 {
