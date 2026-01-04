185fc9d3cb6a258d4c66c330f092a2fd390fce46
nabil4416 <nabil4416@hotmail.com>
2026-01-04 21:27:12 +0100

    add ex6

---
+++ piscine_python/M00/ex6/ft_garden_summary.py
@@ -0,0 +1,7 @@
+def ft_garden_summary():
+    name = input("Enter garden name: ")
+    plants = input("Enter number of plants: ")
+    print("Garden:", name)
+    print("Plants:", plants)
+    print("Status: Growing well!")
+ft_garden_summary()
