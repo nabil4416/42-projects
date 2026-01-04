30c8b002a6d2c972f3f7d787bb9fb82791bf67d9
nabil4416 <nabil4416@hotmail.com>
2026-01-04 20:43:18 +0100

    add ex5 iterative

---
+++ piscine_python/M00/ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,6 @@
+def ft_count_harvest_iterative():
+    days = int(input("Days until harvest: "))
+    for x in range(1, days + 1):
+        print("Day", x)
+    print("Harvest time!")
+ft_count_harvest_iterative()
